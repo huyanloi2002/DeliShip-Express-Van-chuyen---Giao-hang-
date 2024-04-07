e794319400b1b20fbf10a3d735aa4edb6234acbd
Bùi Đoàn Quang Huy <85773586+huyanloi2002@users.noreply.github.com>
2024-04-07 19:12:19 +0700

    Update Wallpapper.jsx

--- client/src/components/Wallpapper.jsx
+++ client/src/components/Wallpapper.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import wallpapper from "../assets/wallpapper1.png";
 import "../styles/Wallpapper.scss";
 import { Link } from "react-router-dom";
+import {useSelector} from "react-redux"
 
 const Wallpapper = () => {
+  const auth = useSelector(state=> state.auth)
+  const {role} = auth
   return (
     <div className="wallpapper">
       <div className="wallpapper_container">
@@ -15,12 +18,16 @@ const Wallpapper = () => {
             <b>giao hàng thuận tiện</b>
           </p>
 
-          <div className="btn_create_parcel">
+          {role !== "admin" ? <div className="btn_create_parcel">
             <Link to="/create_order">
               <p className="text_add_new_parcel">Thêm một kiện hàng mới</p>
               <i className="fa-solid fa-square-plus"></i>
             </Link>
           </div>
+            :
+            <div className="btn_create_parcel">
+              <p className="text_add_new_parcel">Bạn là admin nên không thể thêm đơn hàng!</p>
+          </div>}
         </div>
       </div>
     </div>
